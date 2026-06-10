add46185eccdf222f3ed590de34cc3c792198f85
Joseph UM <joseph.ugalde.murillo@gmail.com>
2026-06-09 21:07:11 -0600

    - Agregado link a Diagrama de clases
    - Agregadas funciones faltantes a Pasaporte.java

--- src/Ejercicios/Ejercicio_Relaciones_Objetos/Pasaporte.java
+++ src/Ejercicios/Ejercicio_Relaciones_Objetos/Pasaporte.java
@@ -21,43 +21,7 @@ public class Pasaporte {
         this.estaVigente = estaVigente;
     }
 
-    public String getNumero() {
-        return numero;
-    }
-
-    public String getNacionalidad() {
-        return nacionalidad;
-    }
-
-    public String getFechaEmision() {
-        return fechaEmision;
-    }
-
-    public String getFechaVencimiento() {
-        return fechaVencimiento;
-    }
-
     public boolean getEstaVigente() {
         return estaVigente;
-    }
-
-    public void setNumero(String numero) {
-        this.numero = numero;
-    }
-
-    public void setNacionalidad(String nacionalidad) {
-        this.nacionalidad = nacionalidad;
-    }
-
-    public void setFechaEmision(String fechaEmision) {
-        this.fechaEmision = fechaEmision;
-    }
-
-    public void setFechaVencimiento(String fechaVencimiento) {
-        this.fechaVencimiento = fechaVencimiento;
-    }
-
-    public void setEstaVigente(boolean estaVigente) {
-        this.estaVigente = estaVigente;
     }
 }
